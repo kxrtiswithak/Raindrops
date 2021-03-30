395d824e4202c48e946731b08481bb1d225bd9ee
kxrtiswithak <kxrtiswithak@gmail.com>
2021-03-30 17:00:40 +0100

    add test and code

---
+++ src/main/java/com/raindrops/kurtis/raindrops/Raindrops.java
@@ -0,0 +1,10 @@
+package com.raindrops.kurtis.raindrops;
+
+public class Raindrops {
+    public static String raindrops(int num) {
+        if (num % 3 == 0) {
+            return "Pling";
+        }
+        return null;
+    }
+}
--- src/test/java/com/raindrops/kurtis/RaindropsTest.java
+++ src/test/java/com/raindrops/kurtis/RaindropsTest.java
@@ -13,12 +13,24 @@ public class RaindropsTest {
 
     /**
      * <em>Tests</em> that {@code 'Pling'} is returned when
-     * <p>{@code num} is divisible 3 alone
+     * <p>{@code num} is divisible by 3 alone
      * @param num the number to be tested
      */
     @ParameterizedTest(name = "testing with {0}")
     @DisplayName("should return 'Pling'")
     @CsvSource({"3", "6", "9", "12", "18", "27"})
+    void shouldReturnPling(int num) {
+        Assertions.assertEquals(Raindrops.raindrops(num), "Pling");
+    }
+
+    /**
+     * <em>Tests</em> that {@code 'Plang'} is returned when
+     * <p>{@code num} is divisible 5 alone
+     * @param num the number to be tested
+     */
+    @ParameterizedTest(name = "testing with {0}")
+    @DisplayName("should return 'Plang'")
+    @CsvSource({"5", "10", "20", "25", "40", "50"})
     void shouldReturnPling(int num) {
         Assertions.assertEquals(Raindrops.raindrops(num), "Pling");
     }
