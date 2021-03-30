b4b94c53a8b5258f805e6976ef170d1317e30aaa
kxrtiswithak <kxrtiswithak@gmail.com>
2021-03-30 16:46:46 +0100

    add start package

---
+++ src/main/java/com/raindrops/kurtis/start/App.java
@@ -0,0 +1,11 @@
+package com.raindrops.kurtis.start;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        Starter.start();
+    }
+}
---
+++ src/main/java/com/raindrops/kurtis/start/Starter.java
@@ -0,0 +1,9 @@
+package com.raindrops.kurtis.start;
+
+import com.raindrops.kurtis.util.Printer;
+
+public class Starter {
+    public static void start() {
+        Printer.printMessage("message");
+    }
+}
