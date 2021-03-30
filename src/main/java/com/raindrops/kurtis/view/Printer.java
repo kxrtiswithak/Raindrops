90fd11895b19a24b63c5107337011dc0a8aa7cdd
kxrtiswithak <kxrtiswithak@gmail.com>
2021-03-30 18:19:20 +0100

    add one line method to printer

--- src/main/java/com/raindrops/kurtis/view/Printer.java
+++ src/main/java/com/raindrops/kurtis/view/Printer.java
@@ -1,7 +1,11 @@
-package com.raindrops.kurtis.util;
+package com.raindrops.kurtis.view;
 
 public class Printer {
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printMessageOneLine(String message) {
+        System.out.print(message);
     }
 }
