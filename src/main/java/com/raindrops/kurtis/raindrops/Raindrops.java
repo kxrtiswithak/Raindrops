55f6c8a78dd844d0fc738b04f559c8d07c6fcc6f
kxrtiswithak <kxrtiswithak@gmail.com>
2021-03-30 17:20:56 +0100

    add code to pass 'PlingPlang' test

--- src/main/java/com/raindrops/kurtis/raindrops/Raindrops.java
+++ src/main/java/com/raindrops/kurtis/raindrops/Raindrops.java
@@ -2,12 +2,14 @@ package com.raindrops.kurtis.raindrops;
 
 public class Raindrops {
     public static String raindrops(int num) {
-        if (num % 3 == 0) {
+        if (num % 15 == 0) {
+            return "PlingPlang";
+        } else if (num % 7 == 0) {
+            return "Plong";
+        }else if (num % 3 == 0) {
             return "Pling";
         } else if (num % 5 == 0) {
             return "Plang";
-        } else if (num % 7 == 0) {
-            return "Plong";
         }
         return null;
     }
