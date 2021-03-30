09cdb53dee0cc32fcd1c418a63bf2cf37ebfc5f8
kxrtiswithak <kxrtiswithak@gmail.com>
2021-03-30 17:47:04 +0100

    add javadoc comment to raindrops method

--- src/main/java/com/raindrops/kurtis/raindrops/Raindrops.java
+++ src/main/java/com/raindrops/kurtis/raindrops/Raindrops.java
@@ -1,6 +1,19 @@
 package com.raindrops.kurtis.raindrops;
 
 public class Raindrops {
+
+    /**
+     * Returns the appropriate string dependent
+     * <p>on what the string is divisible by:
+     * <ul>
+     *     <li>{@code 'Pling'} for 3</li>
+     *     <li>{@code 'Plang'} for 5</li>
+     *     <li>{@code 'Plong'} for 7</li>
+     * </ul>
+     * <p>a combination is included or omitted where necessary
+     * @param num number to assessed
+     * @return a string adhering to the above criteria
+     */
     public static String raindrops(int num) {
         if (num % 105 == 0) {
             return "PlingPlangPlong";
@@ -12,12 +25,11 @@ public class Raindrops {
             return "PlingPlang";
         } else if (num % 7 == 0) {
             return "Plong";
-        }else if (num % 3 == 0) {
-            return "Pling";
         } else if (num % 5 == 0) {
             return "Plang";
+        }else if (num % 3 == 0) {
+            return "Pling";
         }
-
         return String.valueOf(num);
     }
 }
